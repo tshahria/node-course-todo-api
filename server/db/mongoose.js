4be58c7c60d2b58bbe96c09cb3f3e902188cecf2
Tanvir <34381989+tshahria@users.noreply.github.com>
2018-07-13 16:32:58 -0400

    bug fixes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb:tshahria:randomuser1@ds137611.mlab.com:37611/tanvirsdatabase");
+mongoose.connect("mongodb:gg:lol123@ds137611.mlab.com:37611/tanvirsdatabase");
 
 module.exports = {mongoose};
